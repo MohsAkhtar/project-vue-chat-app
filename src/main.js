112f16ea201b3152f80f8c944d33e652c0393ebe
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-13 13:14:55 +0000

    Added firebase script

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+//Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyAIrpp-PEWA-31mIFDUmnYeDG_bMvjhm8w',
+  authDomain: 'vue-chat-app-5944d.firebaseapp.com',
+  databaseURL: 'https://vue-chat-app-5944d.firebaseio.com',
+  projectId: 'vue-chat-app-5944d',
+  storageBucket: 'vue-chat-app-5944d.appspot.com',
+  messagingSenderId: '26662371447'
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +24,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,23 @@
 <template>
-  <h2>login page</h2>
+  <div>
+    <div class="jumbotron bg-primary text-white text-center">
+      <h2 class="lead display-3">#VUE CHAT</h2>
+      <p> Realtime communication at it's best</p>
+    </div>
+    <div class="container-fluid">
+    <div class="row">
+      <div class="col text-center">
+        <button class="btn btn-outline-danger btn-lg">Login With Google</button>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center mt-5">
+        <button class="btn btn-outline-info btn-lg">Login With Twitter</button>
+      </div>
+    </div>
+  </div>
+  </div>
+
 </template>
 
 <script>
@@ -7,3 +25,9 @@ export default {
   name: 'login'
 };
 </script>
+
+<style>
+.jumbotron {
+  border-radius: 0% !important;
+}
+</style>
