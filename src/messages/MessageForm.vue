cc62a549de5a0c027be25d7b7375233ede8b9b5c
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-14 19:03:48 +0000

    Messages now save to firebase db

--- src/messages/MessageForm.vue
+++ src/messages/MessageForm.vue
@@ -3,13 +3,13 @@
     <div class="messageForm">
       <form>
         <div class="input-group mb-3">
-          <input type="message" id="message" placeholder="Write something" class="form-control mt-3" autofocus>
+          <input v-model.trim="message" type="message" id="message" placeholder="Write something" class="form-control mt-3" autofocus>
 
 
           <div class="input-group-append">
-            <button class="btn btn-primary mt-3">Send</button>
+            <button @click="sendMessage" type="button" class="btn btn-primary mt-3">&nbsp; Send &nbsp;</button>
 
-            <button class="btn btn-warning mt-3">Send</button>
+            <button type="button" class="btn btn-warning mt-3">Upload</button>
           </div>
         </div>
       </form>
@@ -18,8 +18,57 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import auth from 'firebase/auth';
+
 export default {
-  name: 'message-form'
+  name: 'message-form',
+
+  data() {
+    return {
+      message: '',
+      errors: []
+    };
+  },
+
+  computed: {
+    ...mapGetters(['currentChannel', 'currentUser'])
+  },
+
+  methods: {
+    sendMessage() {
+      // construct new message
+      let newMessage = {
+        content: this.message,
+        timestamp: firebase.database.ServerValue.TIMESTAMP,
+        user: {
+          name: this.currentUser.displayName,
+          avatar: this.currentUser.photoURL,
+          id: this.currentUser.uid
+        }
+      };
+
+      // use some form validation
+      if (this.currentChannel !== null) {
+        if (this.message.length > 0) {
+          // '$parent' refers to parent component (Messages)
+          // Using current channel id for message id's
+          // Note: push() actually gives an item its own unique id in fb database
+          this.$parent.messagesRef
+            .child(this.currentChannel.id)
+            .push()
+            .set(newMessage)
+            .then(() => {})
+            .catch(error => {
+              this.errors.push(error.message);
+            });
+
+          // reset message
+          this.message = '';
+        }
+      }
+    }
+  }
 };
 </script>
 
