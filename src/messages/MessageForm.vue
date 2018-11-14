6ff49cd61d73b5013f68014d22299afc05fe0492
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-14 18:39:09 +0000

    Added message bar to Message form component

--- src/messages/MessageForm.vue
+++ src/messages/MessageForm.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    form
+    <div class="messageForm">
+      <form>
+        <div class="input-group mb-3">
+          <input type="message" id="message" placeholder="Write something" class="form-control mt-3" autofocus>
+
+
+          <div class="input-group-append">
+            <button class="btn btn-primary mt-3">Send</button>
+
+            <button class="btn btn-warning mt-3">Send</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -9,3 +22,22 @@ export default {
   name: 'message-form'
 };
 </script>
+
+<style scoped>
+.messageForm {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 67%;
+  z-index: 100;
+  color: white;
+  text-align: center;
+  margin-bottom: -16px;
+  margin-left: 33.3%;
+}
+
+input,
+button {
+  height: 50px;
+}
+</style>
