7ecab493d248855336c578e4a94b7bed3fa28432
MohsAkhtar <mohs.akhtar@gmail.com>
2018-11-14 20:11:25 +0000

    Added watcher to refresh messages array on channel change

--- src/messages/Messages.vue
+++ src/messages/Messages.vue
@@ -12,6 +12,7 @@
 import SingleMessage from './SingleMessage';
 import MessageForm from './MessageForm';
 import database from 'firebase/database';
+import { mapGetters } from 'vuex';
 
 export default {
   name: 'messages',
@@ -20,8 +21,46 @@ export default {
 
   data() {
     return {
-      messagesRef: firebase.database().ref('messages')
+      messagesRef: firebase.database().ref('messages'),
+      messages: [],
+      channel: ''
     };
+  },
+
+  computed: {
+    ...mapGetters(['currentChannel'])
+  },
+
+  watch: {
+    // watching here so messages are updated asynchronously
+    currentChannel: function() {
+      // if current channel changes, watch for its messages
+      this.messages = [];
+      this.addListeners();
+      this.channel = this.currentChannel;
+    }
+  },
+
+  methods: {
+    addListeners() {
+      // Listen to child added events on current channel
+      this.messagesRef
+        .child(this.currentChannel.id)
+        .on('child_added', snapshot => {
+          this.messages.push(snapshot.val());
+        });
+    },
+
+    detachListeners() {
+      // stop listening on detach
+      if (this.channel !== null) {
+        this.messagesRef.child(this.channel.id).off();
+      }
+    },
+
+    beforeDestroy() {
+      this.detachListeners();
+    }
   }
 };
 </script>
